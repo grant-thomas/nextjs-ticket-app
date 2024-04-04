135b2705c2c0984c1cebaf2feb8efd903479cc6d
grant-thomas <grant3192@aim.com>
2024-04-03 20:04:14 -0500

    Disabled authentication checks to allow creation of first user after deployment.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,16 +1,18 @@
 import UserForm from '@/components/UserForm';
-import React from 'react';
+import React, { useState } from 'react';
 import DataTableSimple from './DataTableSimple';
 import prisma from '@/prisma/db';
 import { getServerSession } from 'next-auth';
 import options from '../api/auth/[...nextauth]/options';
+import { Button } from '@/components/ui/button';
+import { log } from 'console';
 
 const Users = async () => {
-	const session = await getServerSession(options);
+	// const session = await getServerSession(options);
 
-	if (session?.user.role !== 'ADMIN') {
-		return <p className='text-destructive'>Admin access required.</p>;
-	}
+	// if (session?.user.role !== 'ADMIN') {
+	// 	return <p className='text-destructive'>Admin access required.</p>;
+	// }
 
 	const users = await prisma.user.findMany();
 
--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { User } from '@prisma/client';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -19,6 +19,7 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from './ui/select';
+import { log } from 'console';
 
 type UserFormData = z.infer<typeof userSchema>;
 
@@ -30,6 +31,8 @@ const UserForm = ({ user }: Props) => {
 	const [isSubmitting, setIsSubmitting] = useState(false);
 	const [error, setError] = useState('');
 	const router = useRouter();
+
+	const [currentState, setCurrentState] = useState(usePathname());
 
 	const form = useForm<UserFormData>({
 		resolver: zodResolver(userSchema),
@@ -56,102 +59,123 @@ const UserForm = ({ user }: Props) => {
 		}
 	}
 
+	function handleCancel() {
+		if (currentState === '/users') {
+			setCurrentState('');
+		}
+		setCurrentState('/users');
+		router.push('/users');
+	}
+
 	return (
 		<>
-			<div>
-				<div className='rounded-md border w-full p-4'>
-					<Form {...form}>
-						<form
-							onSubmit={form.handleSubmit(onSubmit)}
-							className='space-y-8 w-full'>
-							<FormField
-								control={form.control}
-								defaultValue={user?.name}
-								name='name'
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Full Name</FormLabel>
-										<FormControl>
-											<Input
-												placeholder='Enter Users Full Name...'
-												{...field}
-											/>
-										</FormControl>
-									</FormItem>
-								)}
-							/>
-
-							<FormField
-								control={form.control}
-								defaultValue={user?.username}
-								name='username'
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Username</FormLabel>
-										<FormControl>
-											<Input placeholder='Enter a Username...' {...field} />
-										</FormControl>
-									</FormItem>
-								)}
-							/>
-
-							<FormField
-								control={form.control}
-								defaultValue=''
-								name='password'
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Password</FormLabel>
-										<FormControl>
-											<Input
-												type='password'
-												required={user ? false : true}
-												placeholder='Enter a Password...'
-												{...field}
-											/>
-										</FormControl>
-									</FormItem>
-								)}
-							/>
-
-							<div className='flex w-full space-x-4'>
+			{currentState === '/users' ? (
+				<Button type='button' onClick={() => setCurrentState('')}>
+					Add User
+				</Button>
+			) : (
+				<div>
+					<div className='rounded-md border w-full p-4'>
+						<Form {...form}>
+							<form
+								onSubmit={form.handleSubmit(onSubmit)}
+								className='space-y-8 w-full'>
 								<FormField
 									control={form.control}
-									defaultValue={user?.role}
-									name='role'
+									defaultValue={user?.name}
+									name='name'
 									render={({ field }) => (
 										<FormItem>
-											<FormLabel>Role</FormLabel>
-											<Select
-												onValueChange={field.onChange}
-												defaultValue={field.value}>
-												<FormControl>
-													<SelectTrigger>
-														<SelectValue
-															placeholder='Role...'
-															defaultValue={user?.role}
-														/>
-													</SelectTrigger>
-												</FormControl>
-												<SelectContent>
-													<SelectItem value='USER'>USER</SelectItem>
-													<SelectItem value='TECH'>TECH</SelectItem>
-													<SelectItem value='ADMIN'>ADMIN</SelectItem>
-												</SelectContent>
-											</Select>
+											<FormLabel>Full Name</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='Enter Users Full Name...'
+													{...field}
+												/>
+											</FormControl>
 										</FormItem>
 									)}
 								/>
-							</div>
 
-							<Button type='submit' disabled={isSubmitting}>
-								{user ? 'Update User' : 'Create User'}
-							</Button>
-						</form>
-					</Form>
-					<p className='text-destructive mt-2'>{error}</p>
+								<FormField
+									control={form.control}
+									defaultValue={user?.username}
+									name='username'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Username</FormLabel>
+											<FormControl>
+												<Input placeholder='Enter a Username...' {...field} />
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									defaultValue=''
+									name='password'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Password</FormLabel>
+											<FormControl>
+												<Input
+													type='password'
+													required={user ? false : true}
+													placeholder='Enter a Password...'
+													{...field}
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+
+								<div className='flex w-full space-x-4'>
+									<FormField
+										control={form.control}
+										defaultValue={user?.role}
+										name='role'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Role</FormLabel>
+												<Select
+													onValueChange={field.onChange}
+													defaultValue={field.value}>
+													<FormControl>
+														<SelectTrigger>
+															<SelectValue
+																placeholder='Role...'
+																defaultValue={user?.role}
+															/>
+														</SelectTrigger>
+													</FormControl>
+													<SelectContent>
+														<SelectItem value='USER'>USER</SelectItem>
+														<SelectItem value='TECH'>TECH</SelectItem>
+														<SelectItem value='ADMIN'>ADMIN</SelectItem>
+													</SelectContent>
+												</Select>
+											</FormItem>
+										)}
+									/>
+								</div>
+
+								<Button type='submit' disabled={isSubmitting}>
+									{user ? 'Update User' : 'Create User'}
+								</Button>
+								<Button
+									type='button'
+									disabled={isSubmitting}
+									className='ml-3'
+									onClick={handleCancel}>
+									Cancel
+								</Button>
+							</form>
+						</Form>
+						<p className='text-destructive mt-2'>{error}</p>
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 };
