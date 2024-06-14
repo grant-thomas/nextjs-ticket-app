57a969ee926de67944afdabee77e11a37643ed15
grant-thomas <grant3192@aim.com>
2024-06-14 12:06:48 -0500

    Added authentication back to users endpoint and users page, after admin account was created.

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,15 +7,15 @@ import options from '../auth/[...nextauth]/options';
 import { log } from 'console';
 
 export async function POST(request: NextRequest) {
-	// const session = await getServerSession(options);
+	const session = await getServerSession(options);
 
-	// if (!session) {
-	// 	return NextResponse.json({ error: 'Not authenticated!' }, { status: 401 });
-	// }
+	if (!session) {
+		return NextResponse.json({ error: 'Not authenticated!' }, { status: 401 });
+	}
 
-	// if (session.user.role !== 'ADMIN') {
-	// 	return NextResponse.json({ error: 'Not Admin!' }, { status: 401 });
-	// }
+	if (session.user.role !== 'ADMIN') {
+		return NextResponse.json({ error: 'Not Admin!' }, { status: 401 });
+	}
 
 	const body = await request.json();
 	const validation = userSchema.safeParse(body);
